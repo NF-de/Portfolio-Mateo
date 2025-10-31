c496d83958d4fedffaf3317b3e3740ce4006279f
Fialoux Mateo <fialouxmateo@gmail.com>
2025-10-31 01:06:35 +0100

    Navbar+1ere card

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const scrollContent = document.querySelector('.scroll-content');
-const cards = document.querySelectorAll('.card'); // toutes les cartes
+const cards = document.querySelectorAll('.card, .hero-card');
 
 let currentY = 0;
 let targetY = 0;
@@ -13,23 +13,11 @@ window.addEventListener('scroll', () => {
 function animate() {
   requestAnimationFrame(animate);
 
-  // Lissage du scroll
+
   currentY += (targetY - currentY) * speed;
 
-  // Néon
   scrollContent.style.backgroundPosition = `center ${-currentY}px`;
 
-  // Cartes : même mouvement que le néon
-  cards.forEach((card, index) => {
-    let offset = 0;
-
-    // Décalage vertical initial selon la position (zigzag)
-    if(index === 0) offset = -50; // première carte à gauche
-    else if(index === 1) offset = 50; // deuxième carte à droite
-    else offset = 0; // cartes suivantes au centre
-
-    card.style.transform = `translateY(${offset - currentY}px)`;
-  });
 }
 
 animate();
